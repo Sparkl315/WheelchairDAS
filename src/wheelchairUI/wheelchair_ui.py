0973f9e9bfafa76074692668344b19d07e3032b9
Austinkuet <128794022+Austinkuet@users.noreply.github.com>
2023-06-23 20:02:06 +0800

    Update 23/6 wheelchair_ui.py

--- src/wheelchairUI/wheelchair_ui.py
+++ src/wheelchairUI/wheelchair_ui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-BUFFER = 100
+BUFFER = 1
 
 class images:   # Image class containing image paths
     wheelchair          = "images/wheelchair.png"
@@ -16,11 +16,25 @@ def close_window():
 def change_image(image):
     return tk.PhotoImage(file=image)
 
+import time
+
+def show_image3():
+    if BUFFER < 10:
+        # Hide the first image and show the second image
+        image_label2.configure(image=None)
+        image_label3.place(relx=0.146, rely=0.4)
+
+def show_image5():
+    if BUFFER < 10:
+        # Hide the first image and show the second image
+        image_label4.configure(image=None)
+        image_label5.place(relx=0.425, rely=0.4)
+
 root = tk.Tk()
 root.attributes("-fullscreen", True)
 
 
-# Load the first image
+# Load the wheeelchair image
 photo = tk.PhotoImage(file=images.wheelchair)
 
 # Create a label with the image
@@ -28,7 +42,8 @@ image_label = tk.Label(root, image=photo)
 image_label.pack(side=tk.LEFT)
 image_label.place(relx=0.2, rely=0.2)
 
-# Load the second image
+
+# Load left middle green image
 photo2 = tk.PhotoImage(file=images.left_middle_green)
 
 # Create a label with the image
@@ -36,17 +51,34 @@ image_label2 = tk.Label(root, image=photo2)
 image_label2.pack(side=tk.LEFT)
 image_label2.place(relx=0.146, rely=0.4)
 
-# Load the second image
-photo3 = tk.PhotoImage(file=images.right_middle_green)
+
+# Load left middle red image
+photo3 = tk.PhotoImage(file=images.left_middle_red)
 
 # Create a label with the image
 image_label3 = tk.Label(root, image=photo3)
-image_label3.pack(side=tk.LEFT)
-image_label3.place(relx=0.425, rely=0.4)
+image_label3.place_forget()
 
+# Load the right middle green image
+photo4 = tk.PhotoImage(file=images.right_middle_green)
+
+# Create a label with the image
+image_label4 = tk.Label(root, image=photo4)
+image_label4.pack(side=tk.LEFT)
+image_label4.place(relx=0.425, rely=0.4)
+
+# Load left middle red image
+photo5 = tk.PhotoImage(file=images.right_middle_red)
+
+# Create a label with the image
+image_label5 = tk.Label(root, image=photo5)
+image_label5.place_forget()
+
+# Schedule the show_image2() function to run every 1 second
+root.after(1000, show_image3)
+root.after(2000, show_image5)
 
 # Add a close button
-
 close_button = tk.Button(root, text="X", command= close_window, font=("Arial", 24), padx=10, pady=5)
 close_button.pack(side= tk.TOP, anchor=tk.NE)
 
